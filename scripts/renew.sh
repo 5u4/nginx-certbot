e38d344cc8a851616455fea079daad85f338b7d2
Sen Hung Wong <alexwongsenhung@gmail.com>
2019-07-16 18:02:17 -0700

    Fix renew script

--- scripts/renew.sh
+++ scripts/renew.sh
@@ -1,3 +1,14 @@
+# Stop main nginx
+docker stop nginx
+
+# Start a temp nginx server that accepts the challenge on port 80
+docker run -it --name temp-nginx -p 80:80 -p 443:443 \
+    -v $(pwd)/temp/default.conf:/etc/nginx/conf.d/default.conf \
+    -v $(pwd)/dhparam/dhparam-2048.pem:/etc/ssl/certs/dhparam-2048.pem \
+    -v $(pwd)/sites:/usr/share/nginx/html \
+    -d nginx:alpine
+
+# Renew certificate with certbot
 docker run --rm -it --name certbot \
     -v $(pwd)/certbot/etc/letsencrypt:/etc/letsencrypt \
     -v $(pwd)/certbot/var/lib/letsencrypt:/var/lib/letsencrypt \
@@ -6,4 +17,11 @@ docker run --rm -it --name certbot \
     certbot/certbot renew \
     --webroot -w /data/letsencrypt --quiet
 
-docker kill --signal=HUP nginx
+# Kill the renew
+docker kill --signal=HUP temp-nginx
+
+# Remove temp nginx server
+docker rm temp-nginx -f
+
+# Restart nginx
+docker start nginx
